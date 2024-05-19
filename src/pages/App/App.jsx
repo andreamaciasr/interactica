9f705785542eb0f65dcbbae1912a9e25bd091fe5
Lucy Ruiz <lucyruizwa@gmail.com>
2024-05-19 16:30:07 -0700

    update navbar options and change name components

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
-import './App.css';
 import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
+import Dashboard from '../Dashboard/Dashboard';
+import ExperienceDetail from '../ExperienceDetail/ExperienceDetail';
+import './App.css';
+
 import NavBar from '../../components/NavBar/NavBar';
 
 export default function App() {
@@ -17,8 +18,8 @@ export default function App() {
             <NavBar user={user} setUser={setUser} />
             <Routes>
               {/* Route components in here */}
-              <Route path="/orders/new" element={<NewOrderPage />} />
-              <Route path="/orders" element={<OrderHistoryPage />} />
+              <Route path="/experiences/:experienceid" element={<ExperienceDetail />} />
+              <Route path="/experiences" element={<Dashboard />} />
             </Routes>
           </>
           :
