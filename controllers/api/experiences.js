100f77a3792ced87fc3339d021d527f1b871493a
Andrea Macias <andreamacrz@gmail.com>
2024-05-20 14:47:41 -0400

    added controllers functions

---
+++ controllers/api/experiences.js
@@ -0,0 +1,24 @@
+const Experience = require("../../models/experience");
+
+module.exports = {
+  create,
+  getAll,
+};
+
+async function create(req, res) {
+  try {
+    const experience = await Experience.create(req.body);
+    res.json(experience.text);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+}
+
+async function getAll(req, res) {
+  try {
+    const experiences = await Experience.find({});
+    res.json(notes);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+}
