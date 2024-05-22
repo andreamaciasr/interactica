aee4e729515fbdb3f4450ac35832dc12887c1f9f
Andrea Macias <andreamacrz@gmail.com>
2024-05-21 21:26:21 -0400

    dashboard working and fixed singluar/plurals on components

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,13 +1,13 @@
 import ExperienceItem from "../ExperienceItem/ExperienceItem";
 
 export default function Dashboard({ experiences }) {
-  
-
   return (
     <>
-      <ul className="Dashboard">{experiences.map((e, idx) =>  (
-        <ExperienceItem experience={e} key={idx} />
-      ))}</ul>
+      <ul className="Dashboard">
+        {experiences.map((e, idx) => (
+          <ExperienceItem experience={e} key={idx} />
+        ))}
+      </ul>
     </>
   );
 }
